fb2155f736153acea6889e4a6aad595e43cee65c
Braien Machado de Paula <braienmp@outlook.com>
2022-07-01 12:01:16 -0300

    feat: set default font-family and font-weight

--- src/style.ts
+++ src/style.ts
@@ -3,6 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
+    font-family: Lato, sans-serif;
+    font-weight: 400;
     margin: 0;
     padding: 0;
   }
