92c11156045c656c95636c44c1067be57b138b0f
Braien Machado de Paula <braienmp@outlook.com>
2022-07-01 17:06:59 -0300

    style: secondary elements in products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import ProductCard from '../components/ProductCard';
 import IProduct from '../interfaces/Product';
@@ -11,6 +12,68 @@ interface ProductsInfo {
   totalItems: number;
   totalPages: number;
 }
+
+const Main = styled.main`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ResultDiv = styled.div`
+  align-items: center;
+  border-bottom: 2px solid #D5D5D5;
+  color: #888888;
+  display: flex;
+  font-size: 18px;
+  gap: 4px;
+  height: 74px;
+  width: 343px;
+
+  span {
+    color: #1D1D1B;
+    font-weight: 700;
+  }
+`;
+
+const MoreContentButtonContainer = styled.div`
+  align-items: center;
+  color: #888888;
+  display: flex;
+  flex-direction: column;
+  font-size: 18px;
+  gap: 10px;
+  justify-content: center;
+  margin: 30px 0;
+  width: 343px;
+
+  button {
+    background-color: inherit;
+    border-radius: 4px;
+    border: 2px solid #C81A78;
+    color: #C81A78;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 16px;
+    padding: 10px 0;
+    transition-duration: 150ms;
+    width: 100%;
+
+    &:hover {
+      background-color: #C81A78;
+      color: white;
+    }
+  }
+
+  p {
+    display: flex;
+    gap: 4px;
+  }
+
+  span {
+    color: #1D1D1B;
+    font-weight: 700;
+  }
+`;
 
 export default function Products() {
   const [info, setInfo] = useState({} as ProductsInfo);
@@ -37,15 +100,15 @@ export default function Products() {
   return (
     <div>
       <Header />
-      <main>
-        <div>
+      <Main>
+        <ResultDiv>
           <span>49</span>
-          <span>produtos encontrados</span>
-        </div>
+          produtos encontrados
+        </ResultDiv>
         <section>
           <ProductCard product={info.items[0]} />
         </section>
-        <div>
+        <MoreContentButtonContainer>
           <button type="button">Mostrar mais</button>
           <p>
             Exibindo
@@ -54,8 +117,8 @@ export default function Products() {
             <span>48</span>
             produtos no total
           </p>
-        </div>
-      </main>
+        </MoreContentButtonContainer>
+      </Main>
     </div>
   );
 }
