9023cf16e70f9eb4ce429ee0b7d61e633a98587d
Braien Machado de Paula <braienmp@outlook.com>
2022-07-02 12:09:22 -0300

    feat: change pagination dinamically

--- src/components/PaginationButtons.tsx
+++ src/components/PaginationButtons.tsx
@@ -15,6 +15,7 @@ interface PageButtonProps {
 const PageButton = styled(SecondaryButton)<PageButtonProps>`
   background-color: ${(props) => (!props.current ? 'inherit' : '#C81A78')};
   color: ${(props) => (!props.current ? '#C81A78' : 'white')};
+  font-size: 16px;
   height: 38px;
   pointer-events: ${(props) => (!props.current ? 'inherit' : 'none')};
   width: 38px;
@@ -24,14 +25,22 @@ const Container = styled.div`
   align-items: center;
   color: #C81A78;
   display: flex;
-  font-size: 16px;
   gap: 8px;
-  line-height: 9px;
   margin-top: 26.64px;
+  
+  span {
+    font-size: 16px;
+    line-height: 9px;
+  }
+
+  .page-button:nth-of-type(2) {
+    width: 73px;
+  }
 `;
 
 const Button = styled.button`
   align-items: center;
+  background-color: inherit;
   border: none;
   color: #C81A78;
   display: flex;
@@ -39,36 +48,99 @@ const Button = styled.button`
   font-weight: 700;
   gap: 3px;
   line-height: 14.4px;
+  transition-duration: 150ms;
+
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default function PaginationButtons(props: PaginationButtonsProps) {
   const { page, totalPages } = props;
 
-  const generateButtons = () => {
+  const PAGES_BUTTONS_NUMBER = 3;
+
+  const previousButton = () => {
+    if (page > 1) {
+      return (
+        <Button type="button" key="previous-page-btn">
+          <ArrowIcon flip />
+          Anterior
+        </Button>
+      );
+    }
+
+    return null;
+  };
+
+  const pagesBeforeSign = () => {
+    if (page > PAGES_BUTTONS_NUMBER) {
+      return (<span key="pages-before">...</span>);
+    }
+
+    return null;
+  };
+
+  const nextButton = () => {
+    if (page !== totalPages) {
+      return (
+        <Button type="button" key="next-page-btn">
+          Próximo
+          <ArrowIcon flip={undefined} />
+        </Button>
+      );
+    }
+
+    return null;
+  };
+
+  const pagesAfterSign = () => {
+    if (page < totalPages - 3) {
+      return (<span key="pages-after">...</span>);
+    }
+
+    return null;
+  };
+
+  const pages = () => {
     const buttons = [];
-    for (let i = 1; i <= totalPages; i += 1) {
+    const isFirstPage = page === 1;
+    const isLastPage = page === totalPages;
+
+    let startPage = 1;
+    let endPage = totalPages;
+
+    if (isFirstPage) {
+      endPage = startPage + 2;
+    } else if (isLastPage) {
+      startPage = endPage - 2;
+    } else {
+      startPage = page - 1;
+      endPage = page + 1;
+    }
+
+    for (let i = startPage; i <= endPage; i += 1) {
       if (i === page) {
-        buttons.push(<PageButton current key={i}>{i}</PageButton>);
+        buttons.push(<PageButton className="page-button" current key={`${i}-${totalPages}`}>{i}</PageButton>);
       } else {
-        buttons.push(<PageButton key={i}>{i}</PageButton>);
+        buttons.push(<PageButton className="page-button" key={`${i}-${totalPages}`}>{i}</PageButton>);
       }
     }
 
     return buttons;
   };
 
+  const generateButtons = () => ([
+    previousButton(),
+    pagesBeforeSign(),
+    ...pages(),
+    pagesAfterSign(),
+    nextButton(),
+  ]);
+
   return (
     <Container>
-      <Button type="button">
-        <ArrowIcon flip />
-        Anterior
-      </Button>
       { generateButtons() }
-      ...
-      <Button type="button">
-        Próximo
-        <ArrowIcon flip={undefined} />
-      </Button>
     </Container>
   );
 }
