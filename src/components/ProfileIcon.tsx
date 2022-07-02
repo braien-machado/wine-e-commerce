21247773fe04261a7ce9e676b002cba8401562b6
Braien Machado de Paula <braienmp@outlook.com>
2022-07-02 00:32:38 -0300

    feat: style icons

---
+++ src/components/ProfileIcon.tsx
@@ -0,0 +1,106 @@
+import React from 'react';
+
+function ProfileIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="56"
+      height="56"
+      fill="none"
+      viewBox="0 0 56 56"
+    >
+      <path fill="#DEDEDE" d="M0 0H56V57H0z" />
+      <g filter="url(#filter0_d_680_6449)">
+        <path
+          fill="#F5F5F5"
+          d="M0 0H1440V1516H0z"
+          transform="translate(-1131 -15)"
+        />
+        <g filter="url(#filter1_d_680_6449)">
+          <g clipPath="url(#clip0_680_6449)">
+            <path fill="#fff" d="M-1131 -15H309V73.233H-1131z" />
+            <path
+              stroke="#555"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2.208"
+              d="M46.416 46.908c-10.77 10.77-26.063 10.77-36.833 0 2.507-2.507 7.264-5.286 13.632-7.833V33.98s-2.547-.894-2.547-6.367c-2.547 0-2.547-5.094 0-5.094 0-.734-4.266-7.466 1.098-6.368 1.273-5.094 13.828-5.094 15.101 0 .76 3.034-1.14 5.83-1.14 6.368 2.467 0 1.792 5.094.02 5.094 0 5.473-2.344 6.367-2.344 6.367v5.095c6.368 2.547 9.764 4.672 13.013 7.833z"
+            />
+            <path
+              stroke="#555"
+              strokeWidth="2"
+              d="M55 28.114c0 14.955-12.09 27.074-27 27.074S1 43.068 1 28.114C1 13.159 13.09 1.04 28 1.04s27 12.119 27 27.074z"
+            />
+          </g>
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_680_6449"
+          width="1480"
+          height="1557"
+          x="-1151"
+          y="-32.003"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="10" />
+          <feColorMatrix values="0 0 0 0 0.583333 0 0 0 0 0.583333 0 0 0 0 0.583333 0 0 0 0.1 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_680_6449"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_680_6449"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_680_6449"
+          width="1446"
+          height="94.233"
+          x="-1134"
+          y="-18.003"
+          colorInterpolationFilters="sRGB"
+          filterUnits="userSpaceOnUse"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            result="hardAlpha"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          />
+          <feOffset dy="1" />
+          <feGaussianBlur stdDeviation="1.5" />
+          <feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.35 0" />
+          <feBlend
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_680_6449"
+          />
+          <feBlend
+            in="SourceGraphic"
+            in2="effect1_dropShadow_680_6449"
+            result="shape"
+          />
+        </filter>
+        <clipPath id="clip0_680_6449">
+          <path
+            fill="#fff"
+            d="M0 0H1440V88.233H0z"
+            transform="translate(-1131 -16.003)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
+
+export default ProfileIcon;
