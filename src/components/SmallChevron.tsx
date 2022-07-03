aa0616a2e4403e86fe6fb72676633afef0e2cebc
Braien Machado de Paula <braienmp@outlook.com>
2022-07-03 19:56:10 -0300

    style: images and small div

---
+++ src/components/SmallChevron.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function SmallChevronIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="6"
+      height="10"
+      fill="none"
+      viewBox="0 0 6 10"
+    >
+      <path
+        fill="#888"
+        fillRule="evenodd"
+        d="M.11 9.398L4.78 5 .11.602A.358.358 0 01.082.116a.309.309 0 01.456-.03l4.945 4.656a.371.371 0 010 .515L.538 9.913a.309.309 0 01-.456-.03A.352.352 0 010 9.657c0-.095.037-.19.11-.258z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default SmallChevronIcon;
--- src/pages/ProductDetails.tsx
+++ src/pages/ProductDetails.tsx
@@ -1,11 +1,76 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import ICartProduct from '../interfaces/CartProduct';
 import useLocalStorage from '../helpers/useLocalStorage';
 import IProduct from '../interfaces/Product';
 import { getProductById } from '../helpers/api';
 import ArrowIcon from '../components/ArrowIcon';
+import SmallChevronIcon from '../components/SmallChevron';
+
+interface ContainerProps {
+  gap: number;
+}
+
+const Container = styled.div<ContainerProps>`
+  display: flex;
+  gap: ${(props) => `${props.gap}px`};
+`;
+
+const ColumnContainer = styled(Container)`
+  flex-direction: column;
+`;
+
+const BackLink = styled(Link)`
+  align-items: center;
+  color: #111111;
+  display: flex;
+  font-family: 'Titillium Web', sans-serif;
+  font-size: 20px;
+  gap: 16px;
+  height: 24px;
+  line-height: 24px;
+  margin: 31.77px 0 25px 160px;
+  text-decoration: none;
+  width: 93px;
+`;
+
+const Main = styled.main`
+  display: flex;
+  gap: 162px;
+`;
+
+const InfoContainer = styled(Container)`
+  flex-direction: column;
+`;
+
+const ChevronContainer = styled(Container)`
+  align-items: center;
+
+  span {
+    color: #C81A78;
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 20px;
+
+    &:last-of-type {
+      color: #888888;
+      font-weight: 400;
+      line-height: 24px;
+    }
+  }
+`;
+
+const Image = styled.img`
+  height: 579px;
+  width: 381px;
+`;
+
+const Flag = styled.img`
+  height: 16px;
+  width: 16px;
+`;
 
 export default function ProductDetails() {
   const [cart, setCart] = useLocalStorage<null | ICartProduct[]>('cart', null);
@@ -33,41 +98,53 @@ export default function ProductDetails() {
         searchByTerm={setSearchTerm}
         toggleMenu={setIsMenuHidden}
       />
-      <Link to="/">
+      <BackLink to="/">
         <ArrowIcon />
         Voltar
-      </Link>
-      <main>
-        <img src={product.image} alt={product.name} />
-        <div>
-          <span>{'Vinhos > Estados Unidos > Califórnia'}</span>
-          <h1>{product.name}</h1>
+      </BackLink>
+      <Main>
+        <Image src={product.image} alt={product.name} />
+        <InfoContainer gap={48}>
           <div>
-            <img src={product.flag} alt="country flag" />
-            <span>{product.type}</span>
-            <span>{product.classification}</span>
-            <span>{product.volume || product.size}</span>
-            <span>{product.rating}</span>
-            <span>{product.avaliations}</span>
+            <ColumnContainer gap={18}>
+              <ChevronContainer gap={8}>
+                <span>Vinhos</span>
+                <SmallChevronIcon />
+                <span>Estados Unidos</span>
+                <SmallChevronIcon />
+                <span>Califórnia</span>
+              </ChevronContainer>
+              <div>
+                <h1>{product.name}</h1>
+                <div>
+                  <Flag src={product.flag} alt="country flag" />
+                  <span>{product.type}</span>
+                  <span>{product.classification}</span>
+                  <span>{product.volume || product.size}</span>
+                  <span>{product.rating}</span>
+                  <span>{product.avaliations}</span>
+                </div>
+              </div>
+            </ColumnContainer>
           </div>
-        </div>
-        <div>
-          <span>R$ 63,67</span>
-          <span>NÃO SÓCIO R$ 120,95/UN</span>
-        </div>
-        <div>
-          <h2>Comentário do Sommelier</h2>
-          <p>{product.sommelierComment}</p>
-        </div>
-        <div>
           <div>
-            <button type="button">-</button>
-            <span>1</span>
-            <button type="button">+</button>
+            <span>R$ 63,67</span>
+            <span>NÃO SÓCIO R$ 120,95/UN</span>
           </div>
-          <button type="button">Adicionar</button>
-        </div>
-      </main>
+          <div>
+            <h2>Comentário do Sommelier</h2>
+            <p>{product.sommelierComment}</p>
+          </div>
+          <div>
+            <div>
+              <button type="button">-</button>
+              <span>1</span>
+              <button type="button">+</button>
+            </div>
+            <button type="button">Adicionar</button>
+          </div>
+        </InfoContainer>
+      </Main>
     </div>
   );
 }
